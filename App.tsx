2ae98e1825ef1b90bc1b2fe9ebf81546de528827
RickaPrincy <rckprincy@gmail.com>
2025-02-06 06:21:27 +0000

    chore: init project

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return  (
+    <View>
+      <Text style={{color: "white",textAlign:"center", marginTop: 50}}>Hello World</Text>
+    </View>
+  )
+}

a323a5cabb4fb5a2b61aaad66fca28dea81fddf6
Eclisher <hei.harizo@gmail.com>
2025-02-07 09:13:25 +0300

    chore: refactore code src::app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function App() {
+  return  (
+    <View>
+      <Text style={{color: "black",textAlign:"center", marginTop: 50}}>Hello World</Text>
+    </View>
+  )
+}
